7217acb321ca1fd67f432beaf127f607d64d9b85
Fajar Maulana Firdaus <sosispanggang.15@gmail.com>
2018-12-16 10:20:28 +0700

    First Commit, moving from local Repo

---
+++ main.py
@@ -0,0 +1,67 @@
+import face_recognition, cv2, pickle
+# import knn
+
+def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
+    """
+    Recognizes faces in given image using a trained KNN classifier
+    """
+    if knn_clf is None and model_path is None:
+        raise Exception("Harus ada model yang dipakai, bisa menggunakan argumen 'knn_clf' ataupun 'model_path'")
+
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    X_face_locations = face_recognition.face_locations(X_img_path)
+
+    if len(X_face_locations) == 0:
+        return []
+
+    faces_encodings = face_recognition.face_encodings(X_img_path, known_face_locations=X_face_locations)
+
+    # Use KNN model
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    return [(pred, loc) if rec else ("Unidentified", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+if __name__ == '__main__':
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    # process_this_frame = True
+
+    while True:
+        ret, frame = video_capture.read()
+        frame = cv2.flip(frame, 1)
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # if process_this_frame:
+        predictions = predict(rgb_small_frame, model_path="trained_model.clf")
+        # face_names = []
+        for name, (top, right, bottom, left) in predictions:
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            print("- Ada {} di ({}, {})".format(name, left, top))
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
---
+++ trainmodel.py
@@ -0,0 +1,48 @@
+import math
+from sklearn import neighbors
+import os
+import os.path
+import pickle
+import face_recognition
+from face_recognition.face_recognition_cli import image_files_in_folder
+
+def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
+    """
+    Trains a k-nearest neighbors classifier for face recognition.
+    """
+    X = []
+    y = []
+
+    for class_dir in os.listdir(train_dir):
+        if not os.path.isdir(os.path.join(train_dir, class_dir)):
+            continue
+
+        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+            image = face_recognition.load_image_file(img_path)
+            face_location = face_recognition.face_locations(image)
+
+            if len(face_location) != 1:
+                print("Image {} not suitable for training: {}".format(img_path, ("Didn't find a face" if len(face_location) < 1 else "Found more than one face")))
+            else:
+                X.append(face_recognition.face_encodings(image, face_location)[0])
+                y.append(class_dir)
+
+    if n_neighbors is None:
+        n_neighbors = int(round(math.sqrt(len(X))))
+        if verbose:
+            print("Choosing n_neighbors automatically: ", n_neighbors)
+
+    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
+    knn_clf.fit(X, y)
+
+    if model_save_path is not None:
+        with open(model_save_path, 'wb') as f:
+            pickle.dump(knn_clf, f)
+
+    return knn_clf
+
+
+if __name__ == '__main__':
+    print("Training KNN classifier...")
+    classifier = train(train_dir="foto", model_save_path="trained_model.clf", n_neighbors=2, knn_algo='kd_tree')
+    print("Training complete!")
